41ab89e5147f83e3e00ceb65f2203a4c83066752
SabGibson <sabelogibson1@gmail.com>
2023-04-16 11:33:42 +0100

    created product serializer and first product view)

--- ps_site_django/products/views.py
+++ ps_site_django/products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+class LatestProductsLists(APIView):
+    def get(self,requests,format=None):
+        products = Product.objects.all()[:4]
+        serializer = ProductSerializer(products,many=True)
+        return Response(serializer.data)
+    

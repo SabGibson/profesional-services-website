4cc32f39e8ec03fcf3d1387c11f32868e5b04b59
SabGibson <sabelogibson1@gmail.com>
2023-05-28 09:23:05 +0100

    make it better

--- ps_site_django/ps_site_django/settingsprod.py
+++ ps_site_django/ps_site_django/settingsprod.py
@@ -133,7 +133,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 WHITENOISE_ROOT = os.path.join(BASE_DIR, 'whiteroot')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
